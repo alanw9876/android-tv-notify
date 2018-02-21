5bec5be3e2aaecf4d2a7c29ef9e19ab790ea2f95
Alan Wallace <alanwallace@Alan-MacMini.local>
2018-02-21 17:08:27 -0500

    Added  test.js example usage

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Android TV Notifications
 
 
-# Example Use:
+# 
+
+Example Use:
 notify('192.168.1.64', 'Stock Alert', "Stock Price Watch: $118.25 as of "+Date(), "stock.png", "15")
---
+++ examples/test.js
@@ -0,0 +1,4 @@
+var notify = require('../index.js');
+
+var image_url = 'http://st.motortrend.com/uploads/sites/10/2017/11/2018-Jeep-Wrangler-Unlimited-Sahara.jpg';
+notify('192.168.1.77', '2018 Jeep Wrangler', 'This is a sample image of the new 2018 Jeep Wrangler', image_url, 5);
