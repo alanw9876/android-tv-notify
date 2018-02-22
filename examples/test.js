7bc0ca41d3c259ff4e45c39a62bc0ac235738691
Alan Wallace <alanwallace@Alan-MacMini.local>
2018-02-21 19:40:54 -0500

    Another Example

--- examples/test.js
+++ examples/test.js
@@ -2,3 +2,6 @@ var notify = require('../index.js');
 
 var image_url = 'http://st.motortrend.com/uploads/sites/10/2017/11/2018-Jeep-Wrangler-Unlimited-Sahara.jpg';
 notify('192.168.1.77', '2018 Jeep Wrangler', 'This is a sample image of the new 2018 Jeep Wrangler', image_url, 5);
+
+var image_url = 'http://st.motortrend.com/uploads/sites/5/2017/12/2018-Jeep-Wrangler-Rubicon.jpg?interpolation=lanczos-none&fit=around|660:438';
+notify('192.168.1.77', '2018 Jeep Wrangler', 'The First Drive.', image_url, 5);
